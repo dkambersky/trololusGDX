059ab553235e95f44b584441afc20ce99e65435d
David Kamberský <david.kambersky@gmail.com>
2016-10-10 21:11:25 +0100

    Initial commit

---
+++ desktop/src/com/github/trollface_studios/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.github.trollface_studios.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.github.trollface_studios.TrololusGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TrololusGame(), config);
+	}
+}
